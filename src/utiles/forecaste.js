05c0462df1faf5b2d25b5cdc79e942ce0dcfe253
unknown <zvarma11@gmail.com>
2020-04-17 17:24:45 +0530

    footer is updated

--- src/utiles/forecaste.js
+++ src/utiles/forecaste.js
@@ -3,9 +3,9 @@ const forecast=(lan,long,callback)=>{
     const url='http://api.weatherstack.com/current?access_key=e53e43e267023b7de6e0f9dc79a048d9&query='+lan+','+long+'&units=m'
     request({url,json:true},(error,{body})=>{
        if(error){
-          callback("unable to connect to weather stack",undefined)
+          callback("unable to connect to weather stack , check your internet ",undefined)
        } else if(body.error){
-           callback('unable find the location ',undefined)
+           callback('unable to  find the location ',undefined)
        } else{
           callback(undefined,'description :'+body.current.weather_descriptions +'.... it is currently '+body.current.temperature+' temperature out.  humudity:'+body.current.humidity+'. wind speed:'+body.current.wind_speed)
        }
